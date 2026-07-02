c1ae1caacaaf4dd8e0301ad31053c420492499bb
Lawty <lawty871@gmail.com>
2026-07-02 16:08:38 -0600

    agregando variable de entorno

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ templates = Jinja2Templates(directory=str(BASE_DIR))
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
